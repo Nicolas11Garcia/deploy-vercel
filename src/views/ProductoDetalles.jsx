be4a340987721fc24dcd8bc7f26499eadd652097
Nicolas11Garcia <nicolasgarciajimenez12@gmail.com>
2023-12-29 20:00:16 -0300

    no meta tags

--- src/views/ProductoDetalles.jsx
+++ src/views/ProductoDetalles.jsx
@@ -12,7 +12,6 @@ export const ProductoDetalles = () => {
   const [tallaSelected, setTallaSelected] = useState("");
   const [validTallaSelected, setValidTallaSelected] = useState(false);
   const navigate = useNavigate();
-  const metaTag = document.querySelector('meta[property="og:image"]');
 
   const [isImageLoaded, setIsImageLoaded] = useState(true);
 
@@ -37,10 +36,7 @@ export const ProductoDetalles = () => {
       setValidTallaSelected(false);
     }
     window.scrollTo(0, 0);
-    if (metaTag) {
-      // Actualiza el contenido de la etiqueta con la nueva URL de la imagen
-      metaTag.content = "https://picsum.photos/id/866/200/200";
-    }
+
   }, [productId, product, navigate]);
 
   const handleSelectionTallaChange = (e) => {
