0409b68018669e361b243b1b6622148be06e34b6
Nicolas11Garcia <nicolasgarciajimenez12@gmail.com>
2023-12-29 19:20:00 -0300

    Link mostrar imagen Redes

--- src/views/ProductoDetalles.jsx
+++ src/views/ProductoDetalles.jsx
@@ -6,6 +6,7 @@ import { Footer } from "../components/Footer";
 import { WhatsappSVG } from "../components/WhatsappSVG";
 import { ProductosRecomendados } from "../components/products/ProductosRecomendados";
 import { SkeletonImage } from "../components/ui/skeleton/SkeletonImage";
+import { Helmet } from "react-helmet";
 
 export const ProductoDetalles = () => {
   const { id } = useParams();
@@ -21,7 +22,7 @@ export const ProductoDetalles = () => {
   // Busca el producto correspondiente al id en el array products
   const product = products.find((p) => p.id === productId);
   const [mensajeWhatsapp, setMensajeWhatsapp] = useState(
-    "Hola como estas me interesa este producto"
+    "Hola como estas me interesa este producto."
   );
 
   const handleImageLoad = () => {
@@ -70,7 +71,7 @@ export const ProductoDetalles = () => {
       const currentUrl = window.location.href;
 
       // Concatena la URL al mensaje de WhatsApp con un salto de línea
-      const mensajeConUrl = `${mensajeWhatsapp}\n. Producto: ${currentUrl}`;
+      const mensajeConUrl = `${mensajeWhatsapp}\nProducto: ${currentUrl}`;
 
       // Abre el enlace de WhatsApp
       const whatsappLink = `https://api.whatsapp.com/send?phone=+56958826024&text=${encodeURIComponent(
@@ -86,112 +87,120 @@ export const ProductoDetalles = () => {
       {!product ? ( // Verifica si product está definido
         <></>
       ) : (
-        <div className="flex flex-col justify-between min-h-screen">
-          <div className="grid grid-cols-1 px-6 sm:grid-cols-2 gap-6 max-w-6xl w-full mx-auto my-14">
-            <div>
-              {isImageLoaded ? <SkeletonImage /> : null}
-              <Image
-                isZoomed={true}
-                src={product.image}
-                alt={product.name}
-                onLoad={handleImageLoad}
-                className={`${isImageLoaded ? "hidden" : null}`}
-              />
-            </div>
-            <div>
-              {product.name !== "" ? (
-                <h1 className=" text-4xl font-semibold uppercase mb-8">
-                  {product.name}
-                </h1>
-              ) : null}
-              {/*PRECIO */}
-              <div className="flex gap-3">
-                {product.oferta === 1 ? (
-                  <p className="uppercase text-xl text-red-500">
-                    ${product.precio_oferta.toLocaleString("es-CL")}
-                  </p>
-                ) : (
-                  null
-                )}
-                <p
-                  className={`uppercase text-xl ${
-                    product.oferta === 1 ? "line-through text-[#71717A]" : null
-                  }`}
-                >
-                  ${product.price.toLocaleString("es-CL")}
-                </p>
-              </div>
-
-              {/*Horma */}
+        <>
+          <Helmet>
+            <meta property="og:title" content='Tendencias Mym - Producto' />
+            <meta property="og:image" content={product.image} />
+            <meta property="og:url" content={window.location.href} />
+            <meta property="og:type" content="product.item" />
+          </Helmet>
+          <div className="flex flex-col justify-between min-h-screen">
+            <div className="grid grid-cols-1 px-6 sm:grid-cols-2 gap-6 max-w-6xl w-full mx-auto my-14">
               <div>
-                  <p className="text-base mt-4">{product.horma}</p>
+                {isImageLoaded ? <SkeletonImage /> : null}
+                <Image
+                  isZoomed={true}
+                  src={product.image}
+                  alt={product.name}
+                  onLoad={handleImageLoad}
+                  className={`${isImageLoaded ? "hidden" : null}`}
+                />
               </div>
-
-              <div className="flex flex-wrap gap-2 items-center my-14">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-                  />
-                </svg>
-                <p className="text-sm max-w-sm">
-                  Realizamos envíos a todo Chile a través de Starken, Correos de
-                  chile y Chilexpress
-                </p>
-              </div>
-              {/*Tallas*/}
-
-              {product.con_tallas == 1 ? (
-                <div className=" mt-20">
-                  <Select
-                    label="Tallas:"
-                    labelPlacement="outside"
-                    placeholder="Selecciona talla"
-                    disabledKeys={["1"]}
-                    className="max-w-xs"
-                    color={validTallaSelected ? "danger" : null}
-                    onChange={handleSelectionTallaChange}
+              <div>
+                {product.name !== "" ? (
+                  <h1 className=" text-4xl font-semibold uppercase mb-8">
+                    {product.name}
+                  </h1>
+                ) : null}
+                {/*PRECIO */}
+                <div className="flex gap-3">
+                  {product.oferta === 1 ? (
+                    <p className="uppercase text-xl text-red-500">
+                      ${product.precio_oferta.toLocaleString("es-CL")}
+                    </p>
+                  ) : null}
+                  <p
+                    className={`uppercase text-xl ${
+                      product.oferta === 1
+                        ? "line-through text-[#71717A]"
+                        : null
+                    }`}
                   >
-                    {product.tallas.map((talla) => (
-                      <SelectItem
-                        key={talla.toString()}
-                        value={talla.toString()}
-                      >
-                        {talla.toString()}
-                      </SelectItem>
-                    ))}
-                  </Select>
+                    ${product.price.toLocaleString("es-CL")}
+                  </p>
                 </div>
-              ) : null}
-              {/*BUTTON WSP */}
-              <Button
-                onClick={handleConsultaWhatsappClick}
-                color="success"
-                className="text-white w-full mt-8 mb-2"
-                startContent={<WhatsappSVG />}
-              >
-                Consultar por Whatsapp
-              </Button>
-              {validTallaSelected ? (
-                <p className="text-sm text-[#f31260] text-center">
-                  Selecciona alguna talla
-                </p>
-              ) : null}
+
+                {/*Horma */}
+                <div>
+                  <p className="text-base mt-4">{product.horma}</p>
+                </div>
+
+                <div className="flex flex-wrap gap-2 items-center my-14">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+                    />
+                  </svg>
+                  <p className="text-sm max-w-sm">
+                    Realizamos envíos a todo Chile a través de Starken, Correos
+                    de chile y Chilexpress
+                  </p>
+                </div>
+                {/*Tallas*/}
+
+                {product.con_tallas == 1 ? (
+                  <div className=" mt-20">
+                    <Select
+                      label="Tallas:"
+                      labelPlacement="outside"
+                      placeholder="Selecciona talla"
+                      disabledKeys={["1"]}
+                      className="max-w-xs"
+                      color={validTallaSelected ? "danger" : null}
+                      onChange={handleSelectionTallaChange}
+                    >
+                      {product.tallas.map((talla) => (
+                        <SelectItem
+                          key={talla.toString()}
+                          value={talla.toString()}
+                        >
+                          {talla.toString()}
+                        </SelectItem>
+                      ))}
+                    </Select>
+                  </div>
+                ) : null}
+                {/*BUTTON WSP */}
+                <Button
+                  onClick={handleConsultaWhatsappClick}
+                  color="success"
+                  className="text-white w-full mt-8 mb-2"
+                  startContent={<WhatsappSVG />}
+                >
+                  Consultar por Whatsapp
+                </Button>
+                {validTallaSelected ? (
+                  <p className="text-sm text-[#f31260] text-center">
+                    Selecciona alguna talla
+                  </p>
+                ) : null}
+              </div>
             </div>
+
+            <ProductosRecomendados id={id} />
+
+            <Footer />
           </div>
-
-          <ProductosRecomendados id={id} />
-
-          <Footer />
-        </div>
+        </>
       )}
     </>
   );
